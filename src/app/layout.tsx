ca1d879cfebd008a1a6ef5d16a75968453069ea6
Yuvraj <11yuviji@gmail.com>
2025-12-07 14:20:46 -0800

    Fix: Upside-Down Website Flipper
    Fixes #29
    
    Generated by Vector @coder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="upside-down">
       <body
         className={`${inter.variable} font-sans antialiased`}
       >
